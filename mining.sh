26ab5e59fbf9bb69baa9d0aaf1839d5274f237cf
imskaaz <60368219+imskaaz@users.noreply.github.com>
2020-02-22 04:03:38 +0800

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,3 +1,12 @@
+#!/bin/bash 
+
+###################################################################
+#Script Name	: PigyMiners v1.0                                                                                              
+#Description	: I will mine Pigy for you !                                                                                                                                                                        
+#Author       	: IMSKAA                                                
+#Email         	: imskaa.co@gmail.com 
+#Website        : https://pigycoin.org                                          
+###########################################
 export PS1="\[\e[31m\]Welcome to PigyMiners\[\e[m\] \[\e[32m\]\u\[\e[m\] "
 
 ./pigycoin-cli generate 1
